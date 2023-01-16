61b432fb456ff4d207f7805e21d5cb5d18687f0a
João Mangueira <jamjs7@gmail.com>
2023-01-16 11:48:47 -0300

    User estilizado

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -12,6 +12,7 @@ const Home = () => {
   const loadUser = async (userName: string) => {
     setError(false);
     setUser(null);
+
     const res = await fetch(`http://api.github.com/users/${userName}`);
     const data = await res.json();
 
