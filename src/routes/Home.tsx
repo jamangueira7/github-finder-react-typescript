ef51bc9c3366bf71a4bf9d2c19045e04034962c8
João Mangueira <jamjs7@gmail.com>
2023-01-16 11:07:36 -0300

    mostrando em tela os dados

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { UserProps } from '../types/user';
-import Search from "../components/Search";
+import Search from '../components/Search';
 import { useState } from 'react';
 
 const Home = () => {
@@ -9,11 +9,22 @@ const Home = () => {
   const loadUser = async (userName: string) => {
     const res = await fetch(`http://api.github.com/users/${userName}`);
     const data = await res.json();
-    console.log(data);
-  }
+    const { avatar_url, login, location, followers, following } = data;
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+
+    setUser(userData);
+  };
+
   return (
     <div>
       <Search loadUser={loadUser}/>
+      {user && <p>{user.login}</p>}
     </div>
   );
 }
