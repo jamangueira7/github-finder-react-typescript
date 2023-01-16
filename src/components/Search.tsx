6dc8ab0d9b27d9a19260761a5d8ec038965527ed
João Mangueira <jamjs7@gmail.com>
2023-01-16 10:58:06 -0300

    trazendo os dados do usuario

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { BsSearch } from 'react-icons/bs';
-const Search = () => {
+import { useState } from 'react';
+
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>;
+};
+const Search = ({loadUser}: SearchProps) => {
+  const [userName, setUserName] = useState("")
   return (
     <div>
       <h2>Busque por um usuário:</h2>
       <p>Conheça seus melhores repositórios</p>
       <div>
-        <input type="text" placeholder="Digite o nome do usuário"/>
-        <button>
+        <input
+          type="text"
+          placeholder="Digite o nome do usuário"
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <button onClick={() => loadUser(userName)}>
           <BsSearch />
         </button>
       </div>
